9b4aee55aa8cc9a02b9763bae95346601e640ab8
Kris <kriskuiper74@gmail.com>
2020-02-26 17:21:29 +0100

    feat: filter houses by property-type

--- src/components/FilterRadio/index.vue
+++ src/components/FilterRadio/index.vue
@@ -2,25 +2,53 @@
   <section class="filter-radio">
     <h2>Property type</h2>
 
-    <p>
-      <input id="type-house" type="radio" v-model="houseType" value="house">
-      <label for="type-house">House</label>
-    </p>
+    <form @change="handleTypeFilter">
+      <div>
+        <input id="type-house" type="radio"  value="house" name="type-filter">
+        <label for="type-house">House</label>
+      </div>
 
-    <p>
-      <input id="type-appartment" type="radio" v-model="houseType" value="appartment">
-      <label for="type-appartment">Appartment</label>
-    </p>
+      <div>
+        <input id="type-appartment" type="radio" value="appartment" name="type-filter">
+        <label for="type-appartment">Appartment</label>
+      </div>
+
+      <div>
+        <input id="type-all" type="radio" value="all" name="type-filter">
+        <label for="type-all">All houses</label>
+      </div>
+    </form>
 
     <span>type: {{houseType}}</span>
   </section>
 </template>
 
 <script>
+import filterData from '@/lib/filter-data'
+
 export default {
   data() {
     return {
       houseType: ''
+    }
+  },
+
+  methods: {
+    handleTypeFilter(event) {
+      this.setHouseType(event.target.value)
+
+      if (event.target.value === 'all') {
+        return // all data
+      }
+
+      const options = { key: 'type', value: event.target.value }
+      const filteredData = filterData(this.$store.state.houses, options)
+
+      // TODO: make a commit to the store that takes all filtered things and
+      // sets the store data to the new data. Or return all data otherwise.
+    },
+    setHouseType(type) {
+      this.houseType = type
     }
   }
 }
